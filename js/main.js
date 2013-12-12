4f5fccf263e25ee22193c67b6cb62d95b9d0febf
Simon Staszkiewicz <simonstaszkiewicz@gmail.com>
2013-12-11 23:43:17 -0800

    Final updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
+/*
+ * JavaScript Pretty Date
+ * Copyright (c) 2011 John Resig (ejohn.org)
+ * Licensed under the MIT and GPL licenses.
+ */
 
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(date){
+    var diff = (((new Date()).getTime() - date.getTime()) / 1000),
+        day_diff = Math.floor(diff / 86400);
+            
+    if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+        return;
+            
+    return day_diff == 0 && (
+            diff < 60 && "just now" ||
+            diff < 120 && "1 minute ago" ||
+            diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+            diff < 7200 && "1 hour ago" ||
+            diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+        day_diff == 1 && "Yesterday" ||
+        day_diff < 7 && day_diff + " days ago" ||
+        day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+}
+
+// If jQuery is included in the page, adds a jQuery plugin to handle it as well
+if ( typeof jQuery != "undefined" ){
+    jQuery.fn.prettyDate = function(){
+        return this.each(function(){
+            var date = prettyDate(this.title);
+            if ( date )
+                jQuery(this).text( date );
+        });
+    };
+}
 
 $(document).ready(function(e) {
 
-    $('#main-nav ul li a').bind('click', function(e) {
+
+    $('#main-nav ul.nav-links li a').bind('click', function(e) {
          // prevent default anchor click behavior
        e.preventDefault();
 
@@ -17,9 +53,40 @@ $(document).ready(function(e) {
          });
     });
 
+    $('#shows a.expand').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('expanded');
+        $("#shows").toggleClass('minimized');
+        $("#shows h3").toggleClass("hidden");
+    });
+
     $.getJSON("shows.json",function(result){
+        $(result.shows).each(function(){
+            this.date = new Date(this.date);
+        });
+        result.shows.sort(function (a,b) {
+            if(a.date > b.date)
+                return 1;
+            else if(b.date > a.date)
+                return -1;
+            else
+                return 0;
+        });
+        result.shows.reverse();
         $.each(result.shows, function(i, field){
-            $("#shows ul").append("<li class='list-group-item'><span>" + field.date + "</span> - <span>" + field.description + "</span></li>");
+            var options = {
+                weekday: "long", year: "numeric", month: "short",
+                day: "numeric", hour: "2-digit", minute: "2-digit"
+            };
+            var diff = (((new Date()).getTime() - field.date.getTime()) / 1000),
+                day_diff = Math.floor(diff / 86400),
+                dateText;
+                if(day_diff > 31 || diff < 0){
+                    dateText = field.date.toLocaleTimeString("en-us", options)
+                }else{
+                    dateText = prettyDate(field.date)
+                }
+            $("#shows ul").append("<li class='list-group-item'><div class='show-item'><span>" + dateText + "</span> - <span>" + field.description + "</span></div></li>");
         });
     });
 
@@ -31,7 +98,7 @@ $(document).ready(function(e) {
             'transitionIn'  : 'fade',
             'transitionOut' : 'fade',
             'title'         : this.title,
-            'width'     : 680,
+            'width'         : 680,
             'height'        : 495,
             'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
             'type'          : 'swf',
@@ -43,6 +110,8 @@ $(document).ready(function(e) {
 
         return false;
     });
+
+    
 
     $("#bio a.album").fancybox();
 });

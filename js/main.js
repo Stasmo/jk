ed0bc20146195fe2a2d9df4024942f78fd2d9ac0
Simon Staszkiewicz <sstaszkiewicz@ea.com>
2013-12-20 12:55:24 -0800

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-/*
- * JavaScript Pretty Date
- * Copyright (c) 2011 John Resig (ejohn.org)
- * Licensed under the MIT and GPL licenses.
- */
 
-// Takes an ISO time and returns a string representing how
-// long ago the date represents.
-function prettyDate(date){
-    var diff = (((new Date()).getTime() - date.getTime()) / 1000),
-        day_diff = Math.floor(diff / 86400);
-            
-    if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-        return;
-            
-    return day_diff == 0 && (
-            diff < 60 && "just now" ||
-            diff < 120 && "1 minute ago" ||
-            diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-            diff < 7200 && "1 hour ago" ||
-            diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-        day_diff == 1 && "Yesterday" ||
-        day_diff < 7 && day_diff + " days ago" ||
-        day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
-}
-
-// If jQuery is included in the page, adds a jQuery plugin to handle it as well
-if ( typeof jQuery != "undefined" ){
-    jQuery.fn.prettyDate = function(){
-        return this.each(function(){
-            var date = prettyDate(this.title);
-            if ( date )
-                jQuery(this).text( date );
-        });
-    };
-}
 
 $(document).ready(function(e) {
 
@@ -74,19 +39,9 @@ $(document).ready(function(e) {
         });
         result.shows.reverse();
         $.each(result.shows, function(i, field){
-            var options = {
-                weekday: "long", year: "numeric", month: "short",
-                day: "numeric", hour: "2-digit", minute: "2-digit"
-            };
-            var diff = (((new Date()).getTime() - field.date.getTime()) / 1000),
-                day_diff = Math.floor(diff / 86400),
-                dateText;
-                if(day_diff > 31 || diff < 0){
-                    dateText = field.date.toLocaleTimeString("en-us", options)
-                }else{
-                    dateText = prettyDate(field.date)
-                }
-            $("#shows ul").append("<li class='list-group-item'><div class='show-item'><span>" + dateText + "</span> - <span>" + field.description + "</span></div></li>");
+            $("#shows ul").append("<li class='list-group-item'><div class='show-item'><span>"
+                + moment(field.date).format("MMM Do YYYY")
+                + "</span> - <span>" + field.description + "</span></div></li>");
         });
     });
 
@@ -110,8 +65,6 @@ $(document).ready(function(e) {
 
         return false;
     });
-
-    
 
     $("#bio a.album").fancybox();
 });
